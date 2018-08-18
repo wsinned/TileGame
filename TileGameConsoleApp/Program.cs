79f56672499ee71e05e6787a4772916bfaec1ec2
wsinned <wsinned@users.noreply.github.com>
2018-08-18 11:12:54 +0100

    Add project files.

---
+++ TileGameConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TileGameConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
