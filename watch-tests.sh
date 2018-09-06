0736b16856a9c35222beb790a663410b82c02820
Dennis Woodruff <dw@denniswoodruff.uk>
2018-09-06 09:47:33 +0100

    Better repo structure
    Was shamed into tidying up the repo.
    Source ocde in src
    Tests in test

--- watch-tests.sh
+++ watch-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dotnet watch --project ./TileGameTests test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./lcov.info
+dotnet watch --project ./test/TileGameTests test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./lcov.info
